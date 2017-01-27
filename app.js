0570a3154f8915cd4e2f8bd44888f4210d31814e
Jeremy Joynes <jeremyajoynes@gmail.com>
2017-01-26 19:07:35 -0800

    update turn in

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var total = 0;
 var score = 0;
-document.getElementById('form1').addEventListener('click', function (e) {
-    e.preventDefault();
-    var Quiz = (function () {
-        function Quiz(question, answer, choice) {
-            this.question = question;
-            this.answer = answer;
-            this.choice = choice;
-        }
-        Quiz.prototype.prompts = function () {
-            var ans = window.prompt(this.question + "  \n " + this.choice);
-            if (ans == this.answer) {
+var Quiz = (function () {
+    function Quiz(question, answer, choice) {
+        var _this = this;
+        this.prompts = function () {
+            var ans = window.prompt(_this.question + "  \n " + _this.choice);
+            if (ans == _this.answer) {
                 score++;
                 total++;
                 alert("Correct! \n " + score + " / " + total);
@@ -20,21 +15,32 @@ document.getElementById('form1').addEventListener('click', function (e) {
                 alert("Wrong! try again next time \n " + score + " / " + total);
             }
         };
-        ;
-        return Quiz;
-    }());
-    var arr = [];
-    var quiz1 = new Quiz("How far can you walk into the woods? ", "half way", "Choices: half way, ear, why, 42, gravity");
-    arr.push(quiz1);
-    var quiz2 = new Quiz("What did vango cut off?", "ear", "Choices: half way, ear, why, 42, gravity");
-    arr.push(quiz2);
-    var quiz3 = new Quiz("who,what,where,when and _____?", "why", "Choices: half way, ear, why, 42, gravity");
-    arr.push(quiz3);
-    var quiz4 = new Quiz("What is the answer to the universe and everything", "42", "Choices: half way, ear, why, 42, gravity");
-    arr.push(quiz4);
-    var quiz5 = new Quiz("You kick a soccer ball. It goes 10 feet and comes back to you. How is this possible? ", "gravity", "Choices: half way, ear, why, 42, gravity");
-    arr.push(quiz5);
-    //console.log(arr);
+        this.question = question;
+        this.answer = answer;
+        this.choice = choice;
+    }
+    return Quiz;
+}());
+var arr = [];
+var quiz1 = new Quiz("How far can you walk into the woods? ", "half way", "Choices: half way, ear, why, 42, gravity");
+arr.push(quiz1);
+var quiz2 = new Quiz("What did vango cut off?", "ear", "Choices: half way, ear, why, 42, gravity");
+arr.push(quiz2);
+var quiz3 = new Quiz("who,what,where,when and _____?", "why", "Choices: half way, ear, why, 42, gravity");
+arr.push(quiz3);
+var quiz4 = new Quiz("What is the answer to the universe and everything", "42", "Choices: half way, ear, why, 42, gravity");
+arr.push(quiz4);
+var quiz5 = new Quiz("You kick a soccer ball. It goes 10 feet and comes back to you. How is this possible? ", "gravity", "Choices: half way, ear, why, 42, gravity");
+arr.push(quiz5);
+//console.log(arr);
+//localStorage['name'] = arr;
+//var data = localStorage.getItem('name');
+//Sconsole.log(localStorage.key(i));
+// arrAdded();
+localStorage.setItem("names", JSON.stringify(arr));
+var storedquiz = JSON.parse(localStorage.getItem("names"));
+document.getElementById('form1').addEventListener('click', function (e) {
+    e.preventDefault();
     for (var i = 0; i < arr.length; i++) {
         arr[i].prompts();
     }
@@ -42,21 +48,15 @@ document.getElementById('form1').addEventListener('click', function (e) {
     //reset score
     total = 0;
     score = 0;
-    document.getElementById('btn').addEventListener('click', function (e) {
-        e.preventDefault();
-        function arrAdded() {
-            var question = document.getElementById('questionAdd').value;
-            var answers = document.getElementById('ansAdd').value;
-            var choices = document.getElementById('posAnsAdd').value;
-            var quizAdd = new Quiz(question, answers, choices);
-            arr.push(quizAdd);
-        }
-        arrAdded();
-        //localStorage['name'] = arr;
-        //var data = localStorage.getItem('name');
-        //Sconsole.log(localStorage.key(i));
-        // arrAdded();
-        localStorage.setItem("names", JSON.stringify(arr));
-        var storedquiz = JSON.parse(localStorage.getItem("names"));
-    }); //end function
 }); //end event listner function
+document.getElementById('btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    function arrAdded() {
+        var question = document.getElementById('questionAdd').value;
+        var answers = document.getElementById('ansAdd').value;
+        var choices = document.getElementById('posAnsAdd').value;
+        var quizAdd = new Quiz(question, answers, choices);
+        arr.push(quizAdd);
+    }
+    arrAdded();
+}); //end function
--- app.ts
+++ app.ts
@@ -1,82 +1,93 @@
 let total = 0;
 let score = 0;
 
+class Quiz {
+
+    question: string;
+    answer: string;
+    choice: string;
+
+    constructor(question: string, answer: string, choice: string) {
+        this.question = question;
+        this.answer = answer;
+        this.choice = choice;
+    }
+
+
+    prompts = (): void => {
+
+        let ans = window.prompt(`${this.question}  \n ${this.choice}`);
+        if (ans == this.answer) {
+            score++;
+            total++;
+            alert(`Correct! \n ${score} / ${total}`);
+        }
+        else {
+            total++;
+            alert(`Wrong! try again next time \n ${score} / ${total}`);
+        }
+
+    };
+}
+
+let arr: Quiz[] = [];
+
+let quiz1 = new Quiz(
+    "How far can you walk into the woods? ",
+    "half way",
+    "Choices: half way, ear, why, 42, gravity"
+
+);
+arr.push(quiz1);
+
+
+let quiz2 = new Quiz(
+    "What did vango cut off?",
+    "ear",
+    "Choices: half way, ear, why, 42, gravity"
+);
+arr.push(quiz2);
+
+
+let quiz3 = new Quiz(
+    "who,what,where,when and _____?",
+    "why",
+    "Choices: half way, ear, why, 42, gravity"
+);
+arr.push(quiz3);
+
+
+let quiz4 = new Quiz(
+    "What is the answer to the universe and everything",
+    "42",
+    "Choices: half way, ear, why, 42, gravity"
+);
+arr.push(quiz4);
+
+
+let quiz5 = new Quiz(
+    "You kick a soccer ball. It goes 10 feet and comes back to you. How is this possible? ",
+    "gravity",
+    "Choices: half way, ear, why, 42, gravity"
+);
+arr.push(quiz5);
+//console.log(arr);
+
+
+//localStorage['name'] = arr;
+//var data = localStorage.getItem('name');
+
+//Sconsole.log(localStorage.key(i));
+// arrAdded();
+
+localStorage.setItem("names", JSON.stringify(arr));
+var storedquiz = JSON.parse(localStorage.getItem("names"));
+
 
 document.getElementById('form1').addEventListener('click', function (e) {
     e.preventDefault();
 
 
-    class Quiz {
-
-        question: string;
-        answer: string;
-        choice: string;
-
-        constructor(question: string, answer: string, choice: string) {
-            this.question = question;
-            this.answer = answer;
-            this.choice = choice;
-        }
-
-
-        prompts() {
-
-            let ans = window.prompt(`${this.question}  \n ${this.choice}`);
-            if (ans == this.answer) {
-                score++;
-                total++;
-                alert(`Correct! \n ${score} / ${total}`);
-            }
-            else {
-                total++;
-                alert(`Wrong! try again next time \n ${score} / ${total}`);
-            }
-
-        };
-    }
-
-    let arr: Quiz[] = [];
-
-    let quiz1 = new Quiz(
-        "How far can you walk into the woods? ",
-        "half way",
-        "Choices: half way, ear, why, 42, gravity"
-
-    );
-    arr.push(quiz1);
-
-
-    let quiz2 = new Quiz(
-        "What did vango cut off?",
-        "ear",
-        "Choices: half way, ear, why, 42, gravity"
-    );
-    arr.push(quiz2);
-
-
-    let quiz3 = new Quiz(
-        "who,what,where,when and _____?",
-        "why",
-        "Choices: half way, ear, why, 42, gravity"
-    );
-    arr.push(quiz3);
-
-
-    let quiz4 = new Quiz(
-        "What is the answer to the universe and everything",
-        "42",
-        "Choices: half way, ear, why, 42, gravity"
-    );
-    arr.push(quiz4);
-
-
-    let quiz5 = new Quiz(
-        "You kick a soccer ball. It goes 10 feet and comes back to you. How is this possible? ",
-        "gravity",
-        "Choices: half way, ear, why, 42, gravity"
-    );
-    arr.push(quiz5);
-    //console.log(arr);
 
 
     for (let i = 0; i < arr.length; i++) {
@@ -87,31 +98,22 @@ document.getElementById('form1').addEventListener('click', function (e) {
     total = 0;
     score = 0;
 
-    document.getElementById('btn').addEventListener('click', function (e) {
-        e.preventDefault();
-        function arrAdded() {
-            let question = (<HTMLInputElement>document.getElementById('questionAdd')).value;
-            let answers = (<HTMLInputElement>document.getElementById('ansAdd')).value;
-            let choices = (<HTMLInputElement>document.getElementById('posAnsAdd')).value;
-            let quizAdd = new Quiz(question, answers, choices);
-            arr.push(quizAdd);
-        }
-
-        arrAdded();
-
-
-        //localStorage['name'] = arr;
-        //var data = localStorage.getItem('name');
-
-        //Sconsole.log(localStorage.key(i));
-        // arrAdded();
-        // localStorage does not work correctly yet
-        localStorage.setItem("names", JSON.stringify(arr));
-        var storedquiz = JSON.parse(localStorage.getItem("names"));
-
-
-    });//end function
 });//end event listner function
 
+document.getElementById('btn').addEventListener('click', function (e) {
+    e.preventDefault();
 
+    function arrAdded() {
+        let question = (<HTMLInputElement>document.getElementById('questionAdd')).value;
+        let answers = (<HTMLInputElement>document.getElementById('ansAdd')).value;
+        let choices = (<HTMLInputElement>document.getElementById('posAnsAdd')).value;
+        let quizAdd = new Quiz(question, answers, choices);
+        arr.push(quizAdd);
+    }
+
+    arrAdded();
+
+
+
+});//end function
 
